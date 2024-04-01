33b3aa183f4dfe82dc51bb93f300419fab3cf203
Kelvin Lin <kelvin@bu.edu>
2024-04-01 08:41:54 -0400

    added error message if invalid length

--- user/models.py
+++ user/models.py
@@ -1,8 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, redirect
 from passlib.hash import pbkdf2_sha256
 from app import db
 import uuid
 class User:
+    #Creates session after signup
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+    
+
     def signup(self):
         print(request.form)
         # Create user object
@@ -15,5 +23,26 @@ class User:
 
         #Encrypt the password
         user['password'] = pbkdf2_sha256.encrypt(user['password'])
-        db.users.insert_one(user)
-        return jsonify(user), 200
+
+        # Check for existing email
+        if db.users.find_one({"email": user['email']}):
+            return jsonify({"error": "Email address already in use"}), 400
+        if db.users.insert_one(user):
+            return self.start_session(user)
+        return jsonify({"error": "Signup failed"}), 400
+    
+
+    #Signout method
+    def signout(self):
+        session.clear()
+        return redirect('/')
+    
+    # Login method
+    def login(self):
+        user = db.users.find_one({
+            "email": request.form.get('email')
+        })
+        # Verifies email and password with the decrypted password from database
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+        return jsonify({"error": "Invalid login credentials"}), 401
