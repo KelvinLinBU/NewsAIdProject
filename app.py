e60bb64bc37080489bcdbfea2640e7bb9a519717
Jkwokhk <97052176+Jkwokhk@users.noreply.github.com>
2024-03-31 20:53:12 -0400

    added functionalities for signup logic
    Users cannot access dashboard by editing website link using /dashboard

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
+from functools import wraps
 import pymongo
 app = Flask(__name__)
 #generate secret key
@@ -7,13 +8,25 @@ app.secret_key = b'-\x04\xfd\xbb)A\xa2\x06G\x94"f\x00+1L\x1f)'
 #Database
 client = pymongo.MongoClient('localhost', 27017)
 db = client.user_login_system
+
+#Decorate
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+    return wrap
 # Routes
-from user import route
+from user import route 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/dashboard/')
+#intercepts redirecting to dashboard with login
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
