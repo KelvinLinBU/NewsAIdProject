a0af8f3c8d9773f6cd29a0bcc9a78f57ca352aa6
Jkwokhk <97052176+Jkwokhk@users.noreply.github.com>
2024-04-11 21:32:57 -0400

    minor fixes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #generate secret key
 app.secret_key = b'-\x04\xfd\xbb)A\xa2\x06G\x94"f\x00+1L\x1f)'
 
-#Database
+#Initiating Database to localhost
 client = pymongo.MongoClient('localhost', 27017)
 db = client.user_login_system
 
