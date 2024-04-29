5757d498b6f7bd8d2718b8c5440ae2c6ba5ff5f9
Daniel <danieldavydov0@gmail.com>
2024-04-28 23:55:36 -0400

    Updated preview

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -58,11 +58,11 @@ export default function Home() {
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
-                  src="/preview.jpg"
+                  src="/cs411collage.png"
                   width={1200}
                   height={900}
                   alt="NewsAId Preview"
-                  className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                  className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 "
                 />
               </div>
             </div>
